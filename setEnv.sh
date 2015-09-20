e62ea67a6ebb241f6f05ab1d9ca50ab74d99d453
Vikram Godbole <VGodbole@walmartlabs.com>
2015-09-20 06:27:14 -0700

    hack day

---
+++ setEnv.sh
@@ -0,0 +1,8 @@
+export TWILIO_ACCOUNT_SID="AC30fb989c30065e231a94d816bd6178d6"
+export TWILIO_AUTH_TOKEN="a821d934bd1de0f906cb8d6fa6a2307f"
+export CUSTOMER_SERVICE_NUMBER="+15102963363" # this will be a Twilio number
+export TWILIO_NUMBER="+15102963363" # this will be a Twilio number
+export AGENT1_NUMBER="+15109968313" # your phone number
+export AGENT2_NUMBER="+16502741082" # a second phone number for testing
+export BASE_URL="https://1d831dc8.ngrok.io"
+export TWILIO_CALLER_ID="+15102963363"
